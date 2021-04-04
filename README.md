191658e86f62b6a47cac3dcb4574e471fc6ca983
John Sosa-Trustham <john.l.sosa@seagate.com>
2021-04-04 05:50:28 -0700

    Added readme and some comments.

---
+++ README.md
@@ -0,0 +1,4 @@
+# COVID-19 CVS Vaccine Appointment Finder
+
+Search for COVID-19 vaccine appointments at local CVS pharmacies.  When an appointment slot is found, an email is sent to the user via their Gmail account.
+
--- getVaccine.py
+++ getVaccine.py
@@ -1,13 +1,16 @@
 import requests
 import datetime
 import time
-import beepy
 import smtplib
 
 def sendEmail(city):
+	# Gmail username and application specific password.
+	# Passwords can be created at: https://myaccount.google.com/security
     gmail_user = 'someone@gmail.com'
     gmail_password = '#############'
     
+    # Message data.  Note, the SMS email address can be entered in order to receive 
+    # an SMS message instead of an email.  For example, "{insert 10-digit number}@vtext.com"
     sent_from = gmail_user
     to = 'someone@gmail.com'
     text = 'Appointment slot found in ' + city
@@ -25,12 +28,14 @@ def sendEmail(city):
 
 def findAppointment():
     while True:
+    	# Choose the your state.  It must be an uppercase two letter state abbreviation.
         state = 'CA' 
 
         try:
             response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
             payload = response.json()
     
+    		# Choose the local citites to search.  See the payload response for all the available cities.
             cities = ['SANTA CRUZ', 'SCOTTS VALLEY', 'LOS GATOS', 'CAPITOLA', 'APTOS'] 
     
             print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
@@ -41,9 +46,6 @@ def findAppointment():
                 if city in cities:
                     print(city, status)
                     if status != 'Fully Booked':
-                        beepy.beep(sound = 'coin')
-                        beepy.beep(sound = 'coin')
-                        beepy.beep(sound = 'coin')
                         sendEmail(city)                
         except:
             print('Something went wrong fetching and processing the appointment data.')
